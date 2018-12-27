40cd3c38951b053896669b51afbf418e5c1841f3
Daniel Trujillo <ganimedes@firemail.cc>
2018-12-27 17:56:05 +0100

    added studies validations

--- app/models/study.rb
+++ app/models/study.rb
@@ -1,3 +1,5 @@
 class Study < ApplicationRecord
   has_many :user_studies
+
+  validates :name, presence: true
 end
--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -8,4 +8,16 @@ class UserProfile < ApplicationRecord
   has_many :user_skills
   has_many :user_traits
   has_many :user_hobbies
+
+  VISA =      ["Australian Resident", "Student Visa", "Working Holiday Visa"]
+  CONTRACT =  ["Casual", "Contractor", "Full Time", "Internship", "Part Time",
+               "Volunteer", "Work Experience"]
+
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :birth_date, presence: true
+  validates :sex, presence: true, inclusion: { in: %w(Male Female Other) }
+  validates :phone, presence: true, numericality: { only_integer: true }
+  validates :residency_status, presence: true, inclusion: { in: VISA }
+  validates :contract_type, presence: true, inclusion: { in: CONTRACT }
 end
